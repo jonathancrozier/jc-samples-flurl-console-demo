e212c6def73ec71a94f5686413369f49cb92b620
Jonathan Crozier <jdcrozier@gmail.com>
2019-10-24 22:47:51 +0100

    Initial commit.

---
+++ JC.Samples.FlurlConsoleDemo/Models/Todo.cs
@@ -0,0 +1,42 @@
+﻿namespace JC.Samples.FlurlConsoleDemo.Models
+{
+    /// <summary>
+    /// Represents a 'todo' item.
+    /// </summary>
+    public class Todo
+    {
+        #region Properties
+
+        /// <summary>
+        /// The unique ID of the todo item.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The ID of the user the todo item is assigned to.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// The Title/Description of the todo item.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Whether or not the todo item has been completed.
+        /// </summary>
+        public bool Completed { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Overrides the standard 'ToString' method with the title of the todo.
+        /// </summary>
+        /// <returns>The title of the todo with a prefix</returns>
+        public override string ToString() => $"Title: {Title}";
+
+        #endregion
+    }
+}
---
+++ JC.Samples.FlurlConsoleDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using Flurl;
+using Flurl.Http;
+using JC.Samples.FlurlConsoleDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JC.Samples.FlurlConsoleDemo
+{
+    /// <summary>
+    /// Main Program class.
+    /// </summary>
+    class Program
+    {
+        #region Constants
+
+        private const string _apiBaseAddress = "https://jsonplaceholder.typicode.com";
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Getting todos...");
+            Console.WriteLine();
+
+            // Get a list of todos from the 'JSONPlaceholder' API.
+            var todos = await _apiBaseAddress
+                              .AppendPathSegment("todos")
+                              .SetQueryParams(new { userId = 1 })
+                              .AllowAnyHttpStatus()
+                              .GetJsonAsync<IEnumerable<Todo>>();
+
+            // Display the number of todos found.
+            string todoCountMessage = $"Found {todos.Count()} todos";
+
+            Console.WriteLine(todoCountMessage);
+            Console.WriteLine("".PadLeft(todoCountMessage.Length, '='));
+            Console.WriteLine();
+
+            // Output the todo titles.
+            foreach (var todo in todos)
+            {
+                Console.WriteLine(todo.ToString());
+            }
+
+            // Inform the user that the program has completed.
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
